f7aef81f297898e9515bc5e272feb8c9f50b9f6f
Joe Fleming <joe.fleming@gmail.com>
2018-04-16 17:04:10 -0700

    test: add test for number prop encoding

--- tests/01-basic.js
+++ tests/01-basic.js
@@ -2,15 +2,31 @@ var test = require('tape');
 var rison = require('../');
 
 test('basic object encoding', function(t) {
-  t.plan(1);
+  t.plan(2);
 
-  var output = rison.encode({ hello: 'world' });
-  t.equal(output, '(hello:world)');
+  var decoded = { hello: 'world' };
+  var encoded = '(hello:world)';
+
+  t.equal(rison.encode(decoded), encoded);
+  t.deepEqual(rison.decode(encoded), decoded);
 });
 
 test('basic object encoding', function(t) {
-  t.plan(1);
+  t.plan(2);
 
-  var output = rison.encode({ hello: 'odd(characters:)' });
-  t.equal(output, "(hello:'odd(characters:)')");
+  var decoded = { hello: 'odd(characters:)' };
+  var encoded = "(hello:'odd(characters:)')";
+
+  t.equal(rison.encode(decoded), encoded);
+  t.deepEqual(rison.decode(encoded), decoded);
+});
+
+test('basic number encoding, prop is not quoted', function (t) {
+  t.plan(2);
+
+  var decoded = { 1: 'one' };
+  var encoded = '(\'1\':one)';
+
+  t.equal(rison.encode(decoded), encoded);
+  t.deepEqual(rison.decode(encoded), decoded);
 });
