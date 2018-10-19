cf15fcf53cb58b428171282d61381444ebaae317
joe fleming <joe.fleming@gmail.com>
2018-10-19 15:02:50 -0700

    chore: test file formatting

--- tests/06-roundtrip.js
+++ tests/06-roundtrip.js
@@ -1,11 +1,15 @@
+/* eslint no-console: 0 */
 var test = require('tape');
 var rison = require('../');
 
 test('characters should roundtrip through encode_uri/decode_uri', function(t) {
   t.plan(1);
 
-  var originalObject = {" &+~!*()-_.'\",:@$/":" &+~!*()-_.'\",:@$/"}
+  var originalObject = { ' &+~!*()-_.\'",:@$/': ' &+~!*()-_.\'",:@$/' };
   var encodeThenDecode = rison.decode_uri(rison.encode_uri(originalObject));
-  t.deepEqual(encodeThenDecode, originalObject, 'obj -> encode -> decode = obj');
-
+  t.deepEqual(
+    encodeThenDecode,
+    originalObject,
+    'obj -> encode -> decode = obj'
+  );
 });
